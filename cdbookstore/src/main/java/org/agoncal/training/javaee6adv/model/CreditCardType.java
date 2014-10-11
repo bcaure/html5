e4b3a2521e78b08332bb732ce6d721e42212aa91
bcaure <bcaure@gmail.com>
2014-10-11 19:03:28 +0200

    Remove projects

--- cdbookstore/src/main/java/org/agoncal/training/javaee6adv/model/CreditCardType.java
+++
@@ -1,6 +0,0 @@
-package org.agoncal.training.javaee6adv.model;
-
-public enum CreditCardType
-{
-   VISA, MASTER_CARD, AMERICAN_EXPRESS
-}
--- cdbookstore/src/main/java/org/agoncal/training/javaee6adv/rest/RestApplication.java
+++
@@ -1,9 +0,0 @@
-package org.agoncal.training.javaee6adv.rest;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/rest")
-public class RestApplication extends Application
-{
-}
